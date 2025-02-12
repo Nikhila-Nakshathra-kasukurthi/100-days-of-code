e69580abe16d997c7f4d61cb6e87d6cfbbe8bf05
Nikhila-Nakshathra-kasukurthi <nikhila4446@gmail.com>
2025-02-12 22:58:41 +0530

    Add files via upload

---
+++ brand_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city = input("which city did you grow up in?")
+pet = input("What is the name of your pet?")
+print("Your Band name could be: " + city + " " + pet)
